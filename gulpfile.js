0f26c9769d43d4b292c81a83f1b3583e688c5aa6
Maicol Battistini <maicolbattistini@live.it>
2022-01-07 12:03:43 +0100

    impr(types): 🏷️ Aggiunta copia dichiarazioni variabili globali

--- gulpfile.js
+++ gulpfile.js
@@ -1,3 +1,6 @@
+/* eslint-disable unicorn/prefer-module,import/no-extraneous-dependencies,no-undef */
+// noinspection ES6ConvertRequireIntoImport
+
 const gulp = require('gulp');
 const replace = require('gulp-replace');
 
@@ -6,10 +9,19 @@ async function replaceImport() {
     allowEmpty: true
   })
     .pipe(replace(/(?<osm>from ["']openstamanager["'])/, "from '../../../index.js'"))
-    .pipe(gulp.dest('./dist/'))
+    .pipe(gulp.dest('./dist/'));
+}
+
+async function copyGlobals() {
+  gulp.src(["./vendor/devcode-it/openstamanager/resources/js/globals.d.ts"], {
+    allowEmpty: true
+  })
+    .pipe(replace(/(?<osm>from ["']\.\/.+["'])/, "from 'openstamanager'"))
+    .pipe(gulp.dest('./resources/js/'));
 }
 
 exports.default = replaceImport;
 exports.watch = function () {
   gulp.watch('./dist/index.ts', replaceImport);
 }
+exports.copyGlobals = copyGlobals;
